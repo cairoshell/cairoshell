07166f14dc83137a92bec62cf5f89a07e77ca6c7
Paul. F <pcfaguiar@gmail.com>
2016-12-14 21:41:26 -0200

    Read system drives
    Displays all system drives under Cairo Explorer, window starts centered,
    sidebar made scrollable.

--- code/Cairo Desktop/Cairo Explorer/CairoExplorerWindow.xaml.cs
+++ code/Cairo Desktop/Cairo Explorer/CairoExplorerWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 //using System.Windows.Shapes;
 using GlassLib;
+using System.IO;
 
 namespace CairoExplorer
 {
@@ -30,12 +31,59 @@ namespace CairoExplorer
             Dwm.Glass[this].Enabled = true;
             Thickness CairoExplorerBottomBarThickness = new Thickness(0, 0, 0, 25);
             Dwm.Glass[this].Margins = CairoExplorerBottomBarThickness;
+
+            //Update UI
+            UpdateSideBar();
         }
 
         private void OpenBoltMenu(object sender, RoutedEventArgs e)
         {
             this.BoltMenu.IsOpen = true;
             this.BoltMenu.StaysOpen = false;
+        }
+
+        private void UpdateSideBar()
+        {
+            RefreshFavorites();
+            RefreshDiskDrives();
+        }
+
+        private void RefreshFavorites()
+        {
+            //TODO:Read the favorites from database.
+        }
+
+        private void RefreshDiskDrives()
+        {
+            //Add disk drives
+            int baseDrivesIndex = Sidebar.Children.IndexOf(DrivesListSample);
+
+            //Get drives
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            //Get style references
+            Style stackStyle = (Style)this.FindResource("CairoExplorerSidebarSectionHeaderOpen");
+            Style stackTextStyle = (Style)this.FindResource("CairoExplorerSidebarSectionHeaderOpenText");
+
+            //Loop drives
+            for (int i = drives.Length - 1; i >= 0; i--)
+            {
+                string volLabel = drives[i].IsReady ? drives[i].VolumeLabel : drives[i].DriveType.ToString();
+
+                //Only active drives
+                StackPanel panel = new StackPanel();
+                panel.Style = stackStyle;
+
+                TextBlock block = new TextBlock();
+                block.Text = String.Format("{0} ({1})", volLabel, drives[i].Name);
+                block.Style = stackTextStyle;
+                panel.Children.Add(block);
+                Sidebar.Children.Insert(baseDrivesIndex + 1, panel);
+            }//for
+
+            //Remove sample items
+            Sidebar.Children.Remove(DrivesListSample);
+            Sidebar.Children.Remove(DriveTreeView);
         }
     }
 }
