e2c8cc5d4af1632827f879713fdf76c98cad057c
dremin <sam@scj.me>
2017-07-30 01:28:29 -0400

    Remove unused variable

--- code/Cairo Desktop/Cairo Desktop/App.xaml.cs
+++ code/Cairo Desktop/Cairo Desktop/App.xaml.cs
@@ -21,11 +21,7 @@ namespace CairoDesktop
 
             string inner = "";
             if (e.Exception.InnerException != null)
-            {
-                string innerTrace = e.Exception.InnerException.StackTrace;
-
                 inner = "\r\n\r\nInner exception:\r\nMessage: " + e.Exception.InnerException.Message + "\r\nTarget Site: " + e.Exception.InnerException.TargetSite + "\r\n\r\n" + e.Exception.InnerException.StackTrace;
-            }
 
             Common.CairoMessage.Show("Please submit a bug report with the error text below. Thanks! \r\n\r\nMessage: " + e.Exception.Message + "\r\nTarget Site: " + e.Exception.TargetSite + "\r\nVersion: " + version + "\r\n\r\n" + e.Exception.StackTrace + inner, "Oops!  An error has occurred.", MessageBoxButton.OK, MessageBoxImage.Warning);
             e.Handled = true;
