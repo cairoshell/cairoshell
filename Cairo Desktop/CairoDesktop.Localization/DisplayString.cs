62a9aedb53dbbaa901ff980986a481dabdbc764e
dremin <sam@scj.me>
2017-12-05 00:46:54 -0500

    Begin localization support

---
+++ Cairo Desktop/CairoDesktop.Localization/DisplayString.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace CairoDesktop.Localization
+{
+    public class DisplayString
+    {
+        public DisplayString()
+        {
+
+        }
+
+        private static string getString(string stringName)
+        {
+            Dictionary<string, string> lang;
+            bool isDefault = false;
+
+            lang = Language.en_US;
+            isDefault = true;
+
+            if (lang.ContainsKey(stringName))
+                return lang[stringName];
+            else
+            {
+                // default is en_US - return string from there if not found in set language
+                if (!isDefault)
+                {
+                    lang = Language.en_US;
+                    if (lang.ContainsKey(stringName))
+                        return lang[stringName];
+                }
+            }
+
+            return stringName;
+        }
+
+        public static string sProgramsMenu
+        {
+            get
+            {
+                return getString("sProgramsMenu");
+            }
+        }
+
+        public static string sPlacesMenu
+        {
+            get
+            {
+                return getString("sPlacesMenu");
+            }
+        }
+    }
+}
---
+++ Cairo Desktop/CairoDesktop.Localization/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CairoDesktop.Localization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cairo Development Team")]
+[assembly: AssemblyProduct("Cairo Desktop Environment")]
+[assembly: AssemblyCopyright("Copyright © 2017 Cairo Development Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9eea28c8-3af4-4853-a31c-46e6fbf7fbb5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.3.*")]
