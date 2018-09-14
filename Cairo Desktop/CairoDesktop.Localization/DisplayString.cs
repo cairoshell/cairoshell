cd521f62304896a03ead0e408ce594649f4b45c2
zen0bit <kvvark@gmail.com>
2018-09-14 04:06:05 +0200

    Update DisplayString.cs
    cosmetic changes, no function change

--- Cairo Desktop/CairoDesktop.Localization/DisplayString.cs
+++ Cairo Desktop/CairoDesktop.Localization/DisplayString.cs
@@ -11,7 +11,7 @@ namespace CairoDesktop.Localization
             new KeyValuePair<string, string>("Português (Brasil)", "pt_BR"),
             new KeyValuePair<string, string>("Svenska", "sv_SE"),
             new KeyValuePair<string, string>("Chinese (Simplified) 简体中文", "zh_CN"),
-			new KeyValuePair<string, string>("Czech", "cs_CZ"),
+            new KeyValuePair<string, string>("Czech", "cs_CZ"),
             new KeyValuePair<string, string>("German", "de_DE"),
         };
 
