bd0f46e66a48118170200f30fc02ac61ed4665ac
Brummell67 <fabrizio.alessandri.g@gmail.com>
2020-01-12 06:29:42 +0100

    Added RunAtLogOn

--- Cairo Desktop/CairoDesktop.Configuration/Properties/Settings.Designer.cs
+++ Cairo Desktop/CairoDesktop.Configuration/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CairoDesktop.Configuration.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -404,18 +404,6 @@ namespace CairoDesktop.Configuration.Properties {
             }
             set {
                 this["FullWidthTaskBar"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool RunAtLogOn {
-            get {
-                return ((bool)(this["RunAtLogOn"]));
-            }
-            set {
-                this["RunAtLogOn"] = value;
             }
         }
     }
