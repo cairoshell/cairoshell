bce7708628fccfc5847980ab1ee6e9c28b1c958b
iAwesome404 <thxconnor@gmail.com>
2020-01-28 19:21:36 -0600

    Added Interface to Targeting Pack

--- Cairo Desktop/ProshellNT2.TargetingPack/Class1.cs
+++ Cairo Desktop/ProshellNT2.TargetingPack/Class1.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace ProshellNT2.TargetingPack
 {
-    public class Class1
+    public interface I_NT2Addon
     {
+        string Name { get; }
+        Image Icon { get; }
+        void OnRun(Form mainForm);
+        void OnBoot();
+        void OnShutdown();
     }
 }
