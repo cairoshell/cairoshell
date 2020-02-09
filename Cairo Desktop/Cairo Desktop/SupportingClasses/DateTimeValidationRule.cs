ae9389a182532ad521ef203fb7cd70df29141f7c
Joseph LaFountain <josuave83@outlook.com>
2020-02-09 17:24:01 -0500

    Added validation to Settings Date and Time format fields

---
+++ Cairo Desktop/Cairo Desktop/SupportingClasses/DateTimeValidationRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace CairoDesktop.SupportingClasses
+{
+    public class DateTimeFormatValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                if (value is string)
+                {
+                    var foo = DateTime.Now.ToString((string)value);
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    return new ValidationResult(false, "Unrecognized Value.");
+                }
+            }
+            catch (FormatException formatException)
+            {
+                return new ValidationResult(false, "Format Invalid: " + formatException.Message);
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                return new ValidationResult(false, "Out of Range: " + argumentOutOfRangeException.Message);
+            }
+        }
+    }
+}
