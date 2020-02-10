c8934824ae6261549cb81efa392d70cfa09b9d88
Joseph LaFountain <josuave83@outlook.com>
2020-02-09 19:20:52 -0500

    Added validation to Desktop Home setting.

---
+++ Cairo Desktop/Cairo Desktop/SupportingClasses/FilePathValidationRule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace CairoDesktop.SupportingClasses
+{
+    public class FilePathValidationRule : ValidationRule
+    {
+        public bool CheckInvalidPathChars { get; set; }
+
+        public bool CheckPathLength { get; set; }
+
+        public bool RequireFileExists { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                if (value is string)
+                {
+                    string stringValue = (string)value;
+
+                    if (string.IsNullOrWhiteSpace(stringValue))
+                    {
+                        return new ValidationResult(false, "Path is Blank");
+                    }
+
+                    if (CheckPathLength)
+                    {
+                        try
+                        {
+                            string fullPath = Path.GetFullPath(stringValue);
+                        }
+                        catch (PathTooLongException pathTooLongException)
+                        {
+                            return new ValidationResult(false, "Path too long: " + pathTooLongException.Message);
+
+                        }
+                    }
+
+                    // Check for Invalid Path Chars.
+                    if (CheckInvalidPathChars)
+                    {
+                        if (Path.GetDirectoryName(stringValue).Any(Path.GetInvalidPathChars().Contains) &&
+                            Path.GetFileName(stringValue).Any(Path.GetInvalidPathChars().Contains))
+                        {
+                            return new ValidationResult(false, "Invalid Path Chars.");
+                        }
+                    }
+
+                    if (RequireFileExists && !File.Exists(stringValue))
+                    {
+                        return new ValidationResult(false, "File doesn't exist.");
+                    }
+
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    return new ValidationResult(false, "Unrecognized Value.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ValidationResult(false, "General Exception: " + ex.Message);
+            }
+        }
+    }
+}
