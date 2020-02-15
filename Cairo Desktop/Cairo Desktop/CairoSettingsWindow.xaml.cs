f868cbe8622007dd61f3847a97ff22755bd16793
dremin <sam@scj.me>
2020-02-14 21:45:24 -0500

    Make background style localizable (#336)

--- Cairo Desktop/Cairo Desktop/CairoSettingsWindow.xaml.cs
+++ Cairo Desktop/Cairo Desktop/CairoSettingsWindow.xaml.cs
@@ -569,10 +569,37 @@
 
             foreach (var backgroundStyleItem in Enum.GetValues(typeof(Desktop.CairoWallpaperStyle)).Cast<Desktop.CairoWallpaperStyle>())
             {
+                string display;
+
+                switch (backgroundStyleItem)
+                {
+                    case Desktop.CairoWallpaperStyle.Center:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Center;
+                        break;
+                    case Desktop.CairoWallpaperStyle.Fill:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Fill;
+                        break;
+                    case Desktop.CairoWallpaperStyle.Fit:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Fit;
+                        break;
+                    case Desktop.CairoWallpaperStyle.Span:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Span;
+                        break;
+                    case Desktop.CairoWallpaperStyle.Stretch:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Stretch;
+                        break;
+                    case Desktop.CairoWallpaperStyle.Tile:
+                        display = Localization.DisplayString.sSettings_Desktop_BackgroundStyle_Tile;
+                        break;
+                    default:
+                        display = backgroundStyleItem.ToString();
+                        break;
+                }
+
                 ComboBoxItem cboItem = new ComboBoxItem()
                 {
                     Tag = backgroundStyleItem,
-                    Content = backgroundStyleItem.ToString()
+                    Content = display
                 };
                 cboWindowsBackgroundStyle.Items.Add(cboItem);
 
