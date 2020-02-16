8b42456c3062c83e130ea0e91e0ff37e8ae9f381
Joseph LaFountain <josuave83@outlook.com>
2020-02-16 11:02:51 -0500

    Added start of GroupPolicy integration. Added RegistryChangeMonitor to watch for registry changes

---
+++ Cairo Desktop/CairoDesktop.Common/Helpers/GroupPolicy.cs
@@ -0,0 +1,68 @@
+﻿using CairoDesktop.Common.DesignPatterns;
+using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Management;
+
+namespace CairoDesktop.Common.Helpers
+{
+    public sealed class GroupPolicyManager : SingletonObject<GroupPolicyManager>, INotifyPropertyChanged, IDisposable
+    {
+        private GroupPolicyManager()
+        {
+            RegistryChangeMonitor registryChangeMonitor = new RegistryChangeMonitor(@"HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            registryChangeMonitor.Changed += RegistryChangeMonitor_Changed;
+            registryChangeMonitor.Start();
+        }
+
+        private void RegistryChangeMonitor_Changed(object sender, RegistryChangeEventArgs e)
+        {
+            // Was this one of our keys?
+        }
+        
+        public void Dispose()
+        {
+            //_watcher.Stop();
+            //_watcher.EventArrived -= HandleEvent;
+            //_watcher.Dispose();
+            //_watcher = null;
+        }
+
+
+        /// <summary>
+        /// Removes icons, shortcuts, and other default and user-defined items from the desktop, including 
+        /// Briefcase, Recycle Bin, Computer, and Network Locations.
+        ///
+        /// Removing icons and shortcuts does not prevent the user from using another method to start the 
+        /// programs or opening the items they represent.
+        ///
+        /// Also, see "Items displayed in Places Bar" in User Configuration\Administrative Templates\Windows 
+        /// Components\Common Open File Dialog to remove the Desktop icon from the Places Bar.This will help 
+        /// prevent users from saving data to the Desktop.
+        /// </summary>
+        /// <remarks>
+        /// Policy: Hide and disable all items on the desktop
+        /// Category Path: User Configuration\Administrative Templates\Desktop\
+        /// Supported On: At least Microsoft Windows 2000
+        /// Registry Key: HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer
+        /// Value: NoDesktop
+        /// Admx: Desktop.admx
+        /// Documentation: https://gpsearch.azurewebsites.net/#146
+        /// </remarks>
+        public bool NoDesktop
+        {
+            get
+            {
+                bool result = false;
+
+                if (Registry.CurrentUser.OpenSubKey(@"\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")?.GetValue("NoDesktop", 0) is int noDesktopValue)
+                    if (noDesktopValue == 1)
+                        result = true;
+
+                return result;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- Cairo Desktop/CairoDesktop.Interop/NativeMethods.cs
+++ Cairo Desktop/CairoDesktop.Interop/NativeMethods.cs
@@ -191,6 +191,32 @@ namespace CairoDesktop.Interop
         [DllImport("advapi32.dll", SetLastError = true)]
         public static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
 
+        [DllImport("Advapi32.dll")]
+        public static extern int RegNotifyChangeKeyValue(IntPtr hKey, bool watchSubtree, REG_NOTIFY_CHANGE notifyFilter, IntPtr hEvent, bool asynchronous);
+
+        [Flags]
+        public enum REG_NOTIFY_CHANGE : uint
+        {
+            /// <summary>
+            /// Notify the caller if a subkey is added or deleted
+            /// </summary>
+            NAME = 0x1,
+            /// <summary>
+            /// Notify the caller of changes to the attributes of the key,
+            /// such as the security descriptor information
+            /// </summary>
+            ATTRIBUTES = 0x2,
+            /// <summary>
+            /// Notify the caller of changes to a value of the key. This can
+            /// include adding or deleting a value, or changing an existing value
+            /// </summary>
+            LAST_SET = 0x4,
+            /// <summary>
+            /// Notify the caller of changes to the security descriptor of the key
+            /// </summary>
+            SECURITY = 0x8
+        }
+
         [DllImport("user32.dll")]
         public static extern bool PostMessage(IntPtr hWnd, uint callback, uint wParam, uint lParam);
 
