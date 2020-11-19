4bc033164543ba04278c91c66d7120dd97754598
dremin <sam@scj.me>
2020-11-18 22:38:31 -0600

    Add support for HideSCAVolume group policy

--- Cairo Desktop/CairoDesktop.Common/Helpers/GroupPolicyManager.cs
+++ Cairo Desktop/CairoDesktop.Common/Helpers/GroupPolicyManager.cs
@@ -5,6 +5,8 @@ namespace CairoDesktop.Common.Helpers
 {
     public sealed class GroupPolicyManager : SingletonObject<GroupPolicyManager>
     {
+        const string ExplorerPolicyKey = @"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer";
+
         private GroupPolicyManager()
         {
         }
@@ -29,20 +31,36 @@ namespace CairoDesktop.Common.Helpers
         /// Admx: Desktop.admx
         /// Documentation: https://gpsearch.azurewebsites.net/#146
         /// </remarks>
-        public bool NoDesktop
+        public bool NoDesktop =>
+            GetRegistryValue<int>(Registry.CurrentUser, ExplorerPolicyKey, "NoDesktop") == 1;
+
+        /// <summary>
+        /// This policy setting allows you to remove the volume control icon from the system control area.
+        /// 
+        /// If you enable this policy setting, the volume control icon is not displayed in the system notification area.
+        ///
+        /// If you disable or do not configure this policy setting, the volume control icon is displayed in the system notification area.
+        /// </summary>
+        /// <remarks>
+        /// Policy: Remove the volume control icon
+        /// Category Path: User Configuration\Administrative Templates\Start Menu and Taskbar\
+        /// Supported On: At least Microsoft Windows Vista
+        /// Registry Key: HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer
+        /// Value: HideSCAVolume
+        /// Admx: Taskbar.admx
+        /// Documentation: https://gpsearch.azurewebsites.net/#4677
+        /// </remarks>
+        public bool HideScaVolume =>
+            GetRegistryValue<int>(Registry.CurrentUser, ExplorerPolicyKey, "HideSCAVolume") == 1;
+
+        private static T GetRegistryValue<T>(RegistryKey key, string subKey, string valueName)
         {
-            get
-            {
-                bool result = false;
-
-                var reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
-                object val = reg?.GetValue("NoDesktop", 0);
-                if (val is int noDesktopValue)
-                    if (noDesktopValue == 1)
-                        result = true;
-
-                return result;
-            }
+            var reg = key?.OpenSubKey(subKey);
+            object val = reg?.GetValue(valueName, default);
+            if (val is T value)
+                return value;
+            
+            return default;
         }
     }
 }
