f06e1684f54b945d2ba07d718f00adfbdaa11949
Joseph LaFountain <josuave83@outlook.com>
2020-11-28 20:20:06 -0500

    Added initial unittest.

---
+++ Cairo Desktop/Tests/Cairo Desktop.Tests/FeelGoodTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace CairoDesktop.Tests
+{
+    public class FeelGoodTests
+    {
+        [Fact]
+        public void Int_Parse_Succeeds()
+        {
+            // Arrange
+            string tenString = "10";
+            int tenInt = 10;
+
+            // Act
+            int result = int.Parse(tenString);
+
+            // Assert
+            Assert.Equal(tenInt, result);
+        }
+    }
+}
