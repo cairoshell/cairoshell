a54247cc2d9b039e0a8de0a798e50d027e1e2190
dremin <sam@scj.me>
2020-12-13 22:03:21 -0600

    Provide exit method to update service via injection

--- Cairo Desktop/CairoDesktop.Infrastructure/Services/WinSparkleUpdateService.cs
+++ Cairo Desktop/CairoDesktop.Infrastructure/Services/WinSparkleUpdateService.cs
@@ -3,7 +3,6 @@ using CairoDesktop.Common;
 using CairoDesktop.Interop;
 using Microsoft.Extensions.Logging;
 using System;
-using System.ComponentModel;
 
 namespace CairoDesktop.Infrastructure.Services
 {
@@ -12,14 +11,24 @@ namespace CairoDesktop.Infrastructure.Services
         private const string UpdateUrl = "https://cairoshell.github.io/appdescriptor.rss";
 
         private readonly ILogger<WinSparkleApplicationUpdateService> _logger;
+        private readonly ICairoApplication _app;
 
-        private bool _isInitialized;
         private WinSparkle.win_sparkle_can_shutdown_callback_t _canShutdownDelegate;
         private WinSparkle.win_sparkle_shutdown_request_callback_t _shutdownDelegate;
 
-        public WinSparkleApplicationUpdateService(ILogger<WinSparkleApplicationUpdateService> logger)
+        public WinSparkleApplicationUpdateService(ILogger<WinSparkleApplicationUpdateService> logger, ICairoApplication app)
         {
             _logger = logger;
+            _app = app;
+
+            WinSparkle.win_sparkle_set_appcast_url(UpdateUrl);
+
+            _canShutdownDelegate = canShutdown;
+            _shutdownDelegate = doShutdown;
+
+            WinSparkle.win_sparkle_set_can_shutdown_callback(_canShutdownDelegate);
+            WinSparkle.win_sparkle_set_shutdown_request_callback(_shutdownDelegate);
+            WinSparkle.win_sparkle_init();
         }
 
         public bool AutomaticUpdatesEnabled
@@ -37,29 +46,6 @@ namespace CairoDesktop.Infrastructure.Services
             }
         }
 
-        public void Initialize(Action shutdownDelegate)
-        {
-            if (_isInitialized) 
-                return;
-
-            if (shutdownDelegate == null)
-            {
-                _logger.LogError("UpdateManager: Unable to initialize; shutdownDelegate is null");
-                return;
-            }
-
-            WinSparkle.win_sparkle_set_appcast_url(UpdateUrl);
-
-            _canShutdownDelegate = canShutdown;
-            _shutdownDelegate = shutdownDelegate.Invoke;
-
-            WinSparkle.win_sparkle_set_can_shutdown_callback(_canShutdownDelegate);
-            WinSparkle.win_sparkle_set_shutdown_request_callback(_shutdownDelegate);
-            WinSparkle.win_sparkle_init();
-
-            _isInitialized = true;
-        }
-
         public void CheckForUpdates()
         {
             WinSparkle.win_sparkle_check_update_with_ui();
@@ -68,6 +54,11 @@ namespace CairoDesktop.Infrastructure.Services
         private int canShutdown()
         {
             return 1;
+        }
+
+        private void doShutdown()
+        {
+            _app.ExitCairo();
         }
 
         protected override void DisposeOfUnManagedResources()
