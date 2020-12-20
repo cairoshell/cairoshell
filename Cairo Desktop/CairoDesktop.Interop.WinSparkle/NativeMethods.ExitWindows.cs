e61021a67e671da1d4db9e637b16e46e4b44ecca
Joseph LaFountain <josuave83@outlook.com>
2020-12-20 09:06:02 -0500

    Moved WinSparkle to its own interop assembly

---
+++ Cairo Desktop/CairoDesktop.Interop.WinSparkle/NativeMethods.ExitWindows.cs
@@ -0,0 +1,29 @@
+﻿namespace CairoDesktop.Interop
+{
+    public partial class NativeMethods
+    {
+        public enum ExitWindows : uint
+        {
+            /// <summary>
+            /// Log the user off.
+            /// </summary>
+            Logoff = 0x00,
+
+            /// <summary>
+            /// Shutdown the machine.
+            /// </summary>
+            Shutdown = 0x08,
+
+            /// <summary>
+            /// Reboots the machine.
+            /// </summary>
+            Reboot = 0x02,
+
+            /// <summary>
+            /// Forces the machine to perform the operation if the apps are hung.
+            /// Use this in conjunction with one of the lower flags.
+            /// </summary>
+            ForceIfHung = 0x10
+        }
+    }
+}
---
+++ Cairo Desktop/CairoDesktop.Interop.WinSparkle/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CairoDesktop.Interop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cairo Development Team")]
+[assembly: AssemblyProduct("Cairo Desktop Environment")]
+[assembly: AssemblyCopyright("Copyright © 2017 Cairo Development Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00be5d8c-6665-4022-aa7a-05d919c5b6c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.3.*")]
