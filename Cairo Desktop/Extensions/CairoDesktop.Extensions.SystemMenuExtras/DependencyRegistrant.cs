41889ff63f9b0117fa7e9cd58b2e4a308f79ce4c
dremin <sam@scj.me>
2020-12-23 19:47:40 -0600

    Misc project cleanup, fix missing export

--- Cairo Desktop/Extensions/CairoDesktop.Extensions.SystemMenuExtras/DependencyRegistrant.cs
+++ Cairo Desktop/Extensions/CairoDesktop.Extensions.SystemMenuExtras/DependencyRegistrant.cs
@@ -1,7 +1,9 @@
-﻿using CairoDesktop.Application.Interfaces;
+﻿using System.ComponentModel.Composition;
+using CairoDesktop.Application.Interfaces;
 
 namespace CairoDesktop.Extensions.SystemMenuExtras
 {
+    [Export(typeof(IDependencyRegistrant))]
     public class DependencyRegistrant : IDependencyRegistrant
     {
         public void Register(IDependencyRegistrar registrar)
