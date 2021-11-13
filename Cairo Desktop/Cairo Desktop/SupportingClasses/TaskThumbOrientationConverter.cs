434435f84286119f186e78d3d12acf9b913b0977
dremin <sam@scj.me>
2021-11-13 12:57:26 -0600

    Don't throw on convertback

--- Cairo Desktop/Cairo Desktop/SupportingClasses/TaskThumbOrientationConverter.cs
+++ Cairo Desktop/Cairo Desktop/SupportingClasses/TaskThumbOrientationConverter.cs
@@ -22,7 +22,7 @@ namespace CairoDesktop.SupportingClasses
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
