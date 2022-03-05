320cc119146e14658826cc034b6ab1e9d9c7d0f4
Matt <me@curoviyx.ru>
2022-03-06 00:27:06 +0300

    WIP: Keyboard layout switcher

--- Cairo Desktop/CairoDesktop.MenuBarExtensions/LayoutSwitcher.xaml.cs
+++ Cairo Desktop/CairoDesktop.MenuBarExtensions/LayoutSwitcher.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ManagedShell.Common.Helpers;
 using System;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
@@ -7,13 +8,28 @@ using System.Windows.Threading;
 
 namespace CairoDesktop.MenuBarExtensions
 {
+    public partial class LayoutEntry
+    {
+        public int Layout { get; set; }
+        public string Name
+        {
+            get
+            {
+                return CultureInfo.GetCultureInfo((short)Layout).NativeName;
+            }
+        }
+    }
+
     public partial class LayoutSwitcher : UserControl
     {
-        private int lastZeroLayout;
-        private int lastForegroundLayout;
+        private int LastZeroLayout;
+        private int LastForegroundLayout;
+        public ObservableCollection<LayoutEntry> CurrentLayouts { get; private set; }
 
         public LayoutSwitcher()
         {
+            CurrentLayouts = new ObservableCollection<LayoutEntry>();
+
             InitializeComponent();
 
             InitializeLayoutIndicator();
@@ -32,37 +48,46 @@ namespace CairoDesktop.MenuBarExtensions
         private void LayoutIndicator_Tick()
         {
             //HACK: I don't why, but GetKeyboardLayout with foreground window PID doesn't update when explorer.exe is focused.
-            //(ex. try to click desktop and try to change layout)
+            //(ex. try to click desktop background and try to change layout)
             //but: with zero PID it updates when explorer.exe or Cairo is focused only.
-            var newZeroLayout = (short) LayoutHelper.GetCurrentLayoutId(true);
-            var newForegroundLayout = (short) LayoutHelper.GetCurrentLayoutId(false);
+            var newZeroLayout = LayoutHelper.GetKeyboardLayout(true);
+            var newForegroundLayout = LayoutHelper.GetKeyboardLayout(false);
 
-            if (newForegroundLayout != lastForegroundLayout)
+            if (newForegroundLayout != LastForegroundLayout)
             {
-                currentLayout.Text = CultureInfo.GetCultureInfo(newForegroundLayout).ThreeLetterISOLanguageName.ToUpper();
-            } else if (newZeroLayout != lastZeroLayout)
+                currentLayout.Text = CultureInfo.GetCultureInfo((short) newForegroundLayout).ThreeLetterISOLanguageName.ToUpper();
+            } else if (newZeroLayout != LastZeroLayout)
             {
-                currentLayout.Text = CultureInfo.GetCultureInfo(newZeroLayout).ThreeLetterISOLanguageName.ToUpper();
+                currentLayout.Text = CultureInfo.GetCultureInfo((short) newZeroLayout).ThreeLetterISOLanguageName.ToUpper();
             }
 
-            lastForegroundLayout = newForegroundLayout;
-            lastZeroLayout = newZeroLayout;
+            LastForegroundLayout = newForegroundLayout;
+            LastZeroLayout = newZeroLayout;
         }
 
         private void LayoutSwitcherItem_SubmenuOpened(object sender, RoutedEventArgs e)
         {
-            
+            CurrentLayouts.Clear();
+            var layouts = LayoutHelper.GetKeyboardLayoutList();
+
+            foreach (var layout in layouts)
+            {
+                CurrentLayouts.Add(new LayoutEntry()
+                {
+                    Layout = (int) layout
+                });
+            }
         }
 
         private void OpenLayoutSettings(object sender, RoutedEventArgs e)
         {
-            //Keyboard layout settings was removed from intl.cpl in Windows 10 (?) 
-            if (EnvironmentHelper.IsWindows10OrBetter)
-            {
-                ShellHelper.StartProcess("ms-settings:keyboard");
-                return;
-            }
-            ShellHelper.StartProcess("intl.cpl");
+            ShellHelper.StartProcess("control", "input.dll");
+        }
+
+        private void ChangeCurrentLayout(object sender, RoutedEventArgs e)
+        {
+            //TODO: PostMessage and LoadKeyboardLayout are inadequate with WPF-based programs. (and Cairo)
+            LayoutHelper.SetKeyboardLayout(((sender as MenuItem).DataContext as LayoutEntry).Layout);
         }
     }
 }
