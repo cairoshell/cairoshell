df38c06fa6e8ec976dd9eab3cf1cfe6a8689040c
Matt <me@curoviyx.ru>
2022-03-06 00:54:35 +0300

    Update for ManagedShell

--- Cairo Desktop/CairoDesktop.MenuBarExtensions/LayoutSwitcher.xaml.cs
+++ Cairo Desktop/CairoDesktop.MenuBarExtensions/LayoutSwitcher.xaml.cs
@@ -5,30 +5,20 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using ManagedShell.Common.Structs;
 
 namespace CairoDesktop.MenuBarExtensions
 {
-    public partial class LayoutEntry
-    {
-        public int Layout { get; set; }
-        public string Name
-        {
-            get
-            {
-                return CultureInfo.GetCultureInfo((short)Layout).NativeName;
-            }
-        }
-    }
 
     public partial class LayoutSwitcher : UserControl
     {
-        private int LastZeroLayout;
-        private int LastForegroundLayout;
-        public ObservableCollection<LayoutEntry> CurrentLayouts { get; private set; }
+        private KeyboardLayout LastZeroLayout;
+        private KeyboardLayout LastForegroundLayout;
+        public ObservableCollection<KeyboardLayout> CurrentLayouts { get; private set; }
 
         public LayoutSwitcher()
         {
-            CurrentLayouts = new ObservableCollection<LayoutEntry>();
+            CurrentLayouts = new ObservableCollection<KeyboardLayout>();
 
             InitializeComponent();
 
@@ -50,15 +40,16 @@ namespace CairoDesktop.MenuBarExtensions
             //HACK: I don't why, but GetKeyboardLayout with foreground window PID doesn't update when explorer.exe is focused.
             //(ex. try to click desktop background and try to change layout)
             //but: with zero PID it updates when explorer.exe or Cairo is focused only.
-            var newZeroLayout = LayoutHelper.GetKeyboardLayout(true);
-            var newForegroundLayout = LayoutHelper.GetKeyboardLayout(false);
+            var newZeroLayout = KeyboardLayoutHelper.GetKeyboardLayout(true);
+            var newForegroundLayout = KeyboardLayoutHelper.GetKeyboardLayout(false);
 
-            if (newForegroundLayout != LastForegroundLayout)
+            if (newForegroundLayout.HKL != LastForegroundLayout.HKL)
             {
-                currentLayout.Text = CultureInfo.GetCultureInfo((short) newForegroundLayout).ThreeLetterISOLanguageName.ToUpper();
-            } else if (newZeroLayout != LastZeroLayout)
+                currentLayout.Text = newForegroundLayout.ThreeLetterName;
+            }
+            else if (newZeroLayout.HKL != LastZeroLayout.HKL)
             {
-                currentLayout.Text = CultureInfo.GetCultureInfo((short) newZeroLayout).ThreeLetterISOLanguageName.ToUpper();
+                currentLayout.Text = newZeroLayout.ThreeLetterName;
             }
 
             LastForegroundLayout = newForegroundLayout;
@@ -68,15 +59,8 @@ namespace CairoDesktop.MenuBarExtensions
         private void LayoutSwitcherItem_SubmenuOpened(object sender, RoutedEventArgs e)
         {
             CurrentLayouts.Clear();
-            var layouts = LayoutHelper.GetKeyboardLayoutList();
-
-            foreach (var layout in layouts)
-            {
-                CurrentLayouts.Add(new LayoutEntry()
-                {
-                    Layout = (int) layout
-                });
-            }
+            var layouts = KeyboardLayoutHelper.GetKeyboardLayoutList();
+            foreach (var layout in layouts) CurrentLayouts.Add(layout);
         }
 
         private void OpenLayoutSettings(object sender, RoutedEventArgs e)
@@ -87,7 +71,7 @@ namespace CairoDesktop.MenuBarExtensions
         private void ChangeCurrentLayout(object sender, RoutedEventArgs e)
         {
             //TODO: PostMessage and LoadKeyboardLayout are inadequate with WPF-based programs. (and Cairo)
-            LayoutHelper.SetKeyboardLayout(((sender as MenuItem).DataContext as LayoutEntry).Layout);
+            KeyboardLayoutHelper.SetKeyboardLayout(((KeyboardLayout)((sender as MenuItem).DataContext)).HKL);
         }
     }
 }
