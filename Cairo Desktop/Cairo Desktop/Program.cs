764414b5fbef6a7e8f01f1ed9cd70339830ccfbe
Sam Johnson <sam@scj.me>
2024-06-07 01:41:48 -0500

    Register inbox commands

--- Cairo Desktop/Cairo Desktop/Program.cs
+++ Cairo Desktop/Cairo Desktop/Program.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using CairoDesktop.Interfaces;
+using CairoDesktop.Commands;
 
 namespace CairoDesktop
 {
@@ -81,6 +82,24 @@ namespace CairoDesktop
 
                     // Inbox extensions below
                     services.AddSingleton<IShellExtension, MenuBarExtensionsShellExtension>();
+
+                    // Inbox commands below
+                    services.AddSingleton<ICairoCommand, AboutCairoCommand>();
+                    services.AddSingleton<ICairoCommand, CheckForUpdatesCommand>();
+                    services.AddSingleton<ICairoCommand, ExitCairoCommand>();
+                    services.AddSingleton<ICairoCommand, HibernateCommand>();
+                    services.AddSingleton<ICairoCommand, LockCommand>();
+                    services.AddSingleton<ICairoCommand, LogOffCommand>();
+                    services.AddSingleton<ICairoCommand, OpenAppGrabberCommand>();
+                    services.AddSingleton<ICairoCommand, OpenCairoSettingsCommand>();
+                    services.AddSingleton<ICairoCommand, OpenControlPanelCommand>();
+                    services.AddSingleton<ICairoCommand, OpenWindowsSettingsCommand>();
+                    services.AddSingleton<ICairoCommand, RestartCommand>();
+                    services.AddSingleton<ICairoCommand, ShowRunDialogCommand>();
+                    services.AddSingleton<ICairoCommand, ShutDownCommand>();
+                    services.AddSingleton<ICairoCommand, SleepCommand>();
+                    services.AddSingleton<ICairoCommand, StartTaskManagerCommand>();
+                    services.AddSingleton<ICairoCommand, ToggleDesktopOverlayCommand>();
                 })
                 .ConfigureLogging((context, logging) =>
                 {
